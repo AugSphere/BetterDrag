ab475a0057b4946e51fa47c691babc9941e1b3cc
AugSphere <augsphere@gmail.com>
2025-10-07 17:19:45 +0200

    Move physics code out of patcher

--- Patcher.cs
+++ Patcher.cs
@@ -68,152 +68,24 @@ namespace BetterDrag
             var transform = ComponentBaseTransform(__instance);
             Profiler.Profile("transform");
 
-            var shipPerformanceData = GetShipData(__instance);
-            Profiler.Profile("shipPerformanceData");
-
             Vector3 velocityVector = ____rb.velocity - waterSurfaceVel;
             Vector3 dragPositionVector = ____rb.position + ____forceHeightOffset * Vector3.up;
             var forwardVector = transform.forward;
             var forwardVelocity = Vector3.Dot(forwardVector, velocityVector);
-            Profiler.Profile("forwardVelocity");
+            Profiler.Profile("velocity");
 
-            var displacement = GetDisplacement(__instance);
-            Profiler.Profile("GetDisplacement");
-
-            var draft = GetDraft(__instance, ____rb);
-            Profiler.Profile("draft");
-
-            var wettedArea =
-                1.7f * shipPerformanceData.LengthAtWaterline * draft + displacement / draft;
-            Profiler.Profile("wettedArea");
-
-            var dragForceMagnitude = CalculateForwardDragForce(
-                forwardVelocity,
-                displacement,
-                wettedArea,
-                shipPerformanceData
+            var dragForceMagnitude = PhysicsCalculation.GetDragForceMagnitude(
+                __instance,
+                ____rb,
+                forwardVelocity
             );
-            Profiler.Profile("dragForceMagnitude");
+            Profiler.Profile("GetDragForceMagnitude");
 
             var dragForceVector = -forwardVector * Mathf.Sign(forwardVelocity) * dragForceMagnitude;
             ____rb.AddForceAtPosition(dragForceVector, dragPositionVector, ForceMode.Force);
             Profiler.Profile("AddForceAtPosition");
 
-#if DEBUG
-            if (DebugCounter.IsAtFirstFrame())
-            {
-                var (testVelocity, testDisplacement, testWettedArea) = (8.5f, 38, 190);
-                FileLog.Log(
-                    $"\nCalling drag function with forwardVelocity:{10}, displacement: {testDisplacement}m^3, wetted area: {testWettedArea}m^2"
-                );
-                CalculateForwardDragForce(
-                    testVelocity,
-                    testDisplacement,
-                    testWettedArea,
-                    shipPerformanceData
-                );
-                FileLog.Log("\n");
-            }
-
-            if (DebugCounter.IsAtPeriod())
-            {
-                FileLog.Log($"{shipPerformanceData}");
-                FileLog.Log($"Draft: {draft}m");
-                FileLog.Log($"Displacement: {displacement}m^3");
-                FileLog.Log($"Wetted area: {wettedArea}m^2");
-                FileLog.Log($"Final modified drag force: {dragForceMagnitude}N");
-                FileLog.Log($"Forward velocity: {forwardVelocity}m/s\n");
-            }
-            DebugCounter.Increment();
-#endif
             Profiler.LogDurations();
-        }
-
-        private static float CalculateForwardDragForce(
-            float forwardVelocity,
-            float displacement,
-            float wettedArea,
-            FinalShipDragPerformanceData performanceData
-        )
-        {
-            var absVelocity = Mathf.Abs(forwardVelocity);
-            var lengthAtWaterline = performanceData.LengthAtWaterline;
-            var formFactor = performanceData.FormFactor;
-
-            return Plugin.globalViscousDragMultiplier!.Value
-                    * performanceData.ViscousDragMultiplier
-                    * performanceData.CalculateViscousDragForce(
-                        absVelocity,
-                        lengthAtWaterline,
-                        formFactor,
-                        displacement,
-                        wettedArea
-                    )
-                + Plugin.globalWaveMakingDragMultiplier!.Value
-                    * performanceData.WaveMakingDragMultiplier
-                    * performanceData.CalculateWaveMakingDragForce(
-                        absVelocity,
-                        lengthAtWaterline,
-                        formFactor,
-                        displacement,
-                        wettedArea
-                    );
-        }
-
-        static (GameObject ship, FinalShipDragPerformanceData data)? cachedShipData;
-
-        private static FinalShipDragPerformanceData GetShipData(BoatProbes instance)
-        {
-            FinalShipDragPerformanceData shipPerformanceData;
-            GameObject ship = instance.gameObject;
-            if (Object.ReferenceEquals(cachedShipData?.ship, ship))
-            {
-                shipPerformanceData = cachedShipData.Value.data;
-            }
-            else
-            {
-                shipPerformanceData = ShipDragDataStore.GetPerformanceData(ship);
-                cachedShipData = (ship, shipPerformanceData);
-#if DEBUG
-                FileLog.Log($"Cache miss for {ship.name}");
-#endif
-            }
-            return shipPerformanceData;
-        }
-
-        static readonly SampleHeightHelper sampleHeightHelper = new();
-        static uint draftSampleCounter = 0;
-        static float lastDraft = 1.0f;
-
-        private static float GetDraft(BoatProbes instance, Rigidbody rb)
-        {
-            draftSampleCounter++;
-            if (draftSampleCounter % Plugin.draftSamplingPeriod!.Value != 0)
-                return lastDraft;
-
-            return SampleDraft(instance, rb);
-        }
-
-        private static float GetDisplacement(BoatProbes instance)
-        {
-            float displacement = 0.0f;
-            for (int idx = 0; idx < instance.appliedBuoyancyForces.Length; idx++)
-                displacement += instance.appliedBuoyancyForces[idx];
-
-            return Mathf.Clamp(displacement * 1e-4f, 0.1f, float.MaxValue);
-        }
-
-        private static float SampleDraft(BoatProbes instance, Rigidbody rb)
-        {
-            var downPoint = rb.ClosestPointOnBounds(rb.centerOfMass + 100 * Vector3.down);
-            sampleHeightHelper.Init(
-                downPoint,
-                instance.ObjectWidth,
-                allowMultipleCallsPerFrame: false
-            );
-            sampleHeightHelper.Sample(out float o_height);
-            lastDraft = Mathf.Clamp(o_height - downPoint.y, 0.1f, float.MaxValue);
-            return lastDraft;
         }
     }
 }
---
+++ PhysicsCalculation.cs
@@ -0,0 +1,145 @@
+﻿using Crest;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BetterDrag
+{
+    internal static class PhysicsCalculation
+    {
+        public static float GetDragForceMagnitude(
+            BoatProbes instance,
+            Rigidbody rb,
+            float forwardVelocity
+        )
+        {
+            var shipPerformanceData = GetShipData(instance);
+            var displacement = GetDisplacement(instance);
+            var draft = GetDraft(instance, rb);
+            var wettedArea =
+                1.7f * shipPerformanceData.LengthAtWaterline * draft + displacement / draft;
+
+            var dragForceMagnitude = CalculateForwardDragForce(
+                forwardVelocity,
+                displacement,
+                wettedArea,
+                shipPerformanceData
+            );
+
+#if DEBUG
+            if (DebugCounter.IsAtFirstFrame())
+            {
+                var (testVelocity, testDisplacement, testWettedArea) = (8.5f, 38, 190);
+                FileLog.Log(
+                    $"\nCalling drag function with forwardVelocity:{10}, displacement: {testDisplacement}m^3, wetted area: {testWettedArea}m^2"
+                );
+                CalculateForwardDragForce(
+                    testVelocity,
+                    testDisplacement,
+                    testWettedArea,
+                    shipPerformanceData
+                );
+                FileLog.Log("\n");
+            }
+
+            if (DebugCounter.IsAtPeriod())
+            {
+                FileLog.Log($"{shipPerformanceData}");
+                FileLog.Log($"Draft: {draft}m");
+                FileLog.Log($"Displacement: {displacement}m^3");
+                FileLog.Log($"Wetted area: {wettedArea}m^2");
+                FileLog.Log($"Final modified drag force: {dragForceMagnitude}N");
+                FileLog.Log($"Forward velocity: {forwardVelocity}m/s\n");
+            }
+            DebugCounter.Increment();
+#endif
+            return dragForceMagnitude;
+        }
+
+        static float CalculateForwardDragForce(
+            float forwardVelocity,
+            float displacement,
+            float wettedArea,
+            FinalShipDragPerformanceData performanceData
+        )
+        {
+            var absVelocity = Mathf.Abs(forwardVelocity);
+            var lengthAtWaterline = performanceData.LengthAtWaterline;
+            var formFactor = performanceData.FormFactor;
+
+            return Plugin.globalViscousDragMultiplier!.Value
+                    * performanceData.ViscousDragMultiplier
+                    * performanceData.CalculateViscousDragForce(
+                        absVelocity,
+                        lengthAtWaterline,
+                        formFactor,
+                        displacement,
+                        wettedArea
+                    )
+                + Plugin.globalWaveMakingDragMultiplier!.Value
+                    * performanceData.WaveMakingDragMultiplier
+                    * performanceData.CalculateWaveMakingDragForce(
+                        absVelocity,
+                        lengthAtWaterline,
+                        formFactor,
+                        displacement,
+                        wettedArea
+                    );
+        }
+
+        static (GameObject ship, FinalShipDragPerformanceData data)? cachedShipData;
+
+        static FinalShipDragPerformanceData GetShipData(BoatProbes instance)
+        {
+            FinalShipDragPerformanceData shipPerformanceData;
+            GameObject ship = instance.gameObject;
+            if (Object.ReferenceEquals(cachedShipData?.ship, ship))
+            {
+                shipPerformanceData = cachedShipData.Value.data;
+            }
+            else
+            {
+                shipPerformanceData = ShipDragDataStore.GetPerformanceData(ship);
+                cachedShipData = (ship, shipPerformanceData);
+#if DEBUG
+                FileLog.Log($"Cache miss for {ship.name}");
+#endif
+            }
+            return shipPerformanceData;
+        }
+
+        static readonly SampleHeightHelper sampleHeightHelper = new();
+        static uint draftSampleCounter = 0;
+        static float lastDraft = 1.0f;
+
+        static float GetDraft(BoatProbes instance, Rigidbody rb)
+        {
+            draftSampleCounter++;
+            if (draftSampleCounter % Plugin.draftSamplingPeriod!.Value != 0)
+                return lastDraft;
+
+            return SampleDraft(instance, rb);
+        }
+
+        static float GetDisplacement(BoatProbes instance)
+        {
+            float displacement = 0.0f;
+            for (int idx = 0; idx < instance.appliedBuoyancyForces.Length; idx++)
+                displacement += instance.appliedBuoyancyForces[idx];
+
+            return Mathf.Clamp(displacement * 1e-4f, 0.1f, float.MaxValue);
+        }
+
+        static float SampleDraft(BoatProbes instance, Rigidbody rb)
+        {
+            var downPoint = rb.ClosestPointOnBounds(rb.centerOfMass + 100 * Vector3.down);
+            sampleHeightHelper.Init(
+                downPoint,
+                instance.ObjectWidth,
+                allowMultipleCallsPerFrame: false
+            );
+            sampleHeightHelper.Sample(out float o_height);
+            lastDraft = Mathf.Clamp(o_height - downPoint.y, 0.1f, float.MaxValue);
+            return lastDraft;
+        }
+    }
+}
