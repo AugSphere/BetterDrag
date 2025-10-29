dcc33e525fe4cbc92b9bced8bb87ebddca5ffa59
AugSphere <augsphere@gmail.com>
2025-10-29 23:39:52 +0100

    Maintain separate draft samplers per ship

--- DraftSampler.cs
+++ DraftSampler.cs
@@ -5,81 +5,94 @@ namespace BetterDrag
 {
     internal static class DraftSampler
     {
-        private static readonly int sampleNumber = 3;
-        private static readonly Vector3[] queryPositions = new Vector3[sampleNumber];
-        private static readonly Vector3[] queryResults = new Vector3[sampleNumber];
-        private static readonly int hashCode = typeof(DraftSampler).GetHashCode();
-        private static uint draftSampleCounter = 0;
-        private static float lastDraft = 1.0f;
-        private static readonly Vector3[] directions =
-        [
-            100 * Vector3.down + 300 * Vector3.forward,
-            100 * Vector3.down,
-            100 * Vector3.down + 300 * Vector3.back,
-        ];
+        static readonly Cache<SamplerImpl> draftSamplers = new("Draft samplers", (_) => new());
 
         internal static float GetAverageDraft(BoatProbes boatProbes, Rigidbody rigidbody)
         {
-            draftSampleCounter++;
-            if (draftSampleCounter % Plugin.draftSamplingPeriod!.Value != 0)
-                return lastDraft;
+            var sampler = draftSamplers.GetValue(rigidbody.gameObject);
+            var averageDraft = sampler.GetAverageDraft(boatProbes, rigidbody);
+            return averageDraft;
+        }
 
-            SetQueryPositions(rigidbody);
+        private class SamplerImpl
+        {
+            private static readonly int sampleNumber = 3;
+            private static readonly Vector3[] directions =
+            [
+                200 * Vector3.forward,
+                Vector3.zero,
+                200 * Vector3.back,
+            ];
+            private readonly Vector3[] queryPositions = new Vector3[sampleNumber];
+            private readonly Vector3[] queryResults = new Vector3[sampleNumber];
+            private uint draftSampleCounter = 0;
+            private float lastDraft = 1.0f;
 
-            float averageDraft = 0f;
-            var queryHash = boatProbes.GetHashCode() ^ hashCode;
-            if (SampleDraft(queryHash, out float seaLevel))
+            internal float GetAverageDraft(BoatProbes boatProbes, Rigidbody rigidbody)
             {
+                draftSampleCounter++;
+                if (draftSampleCounter % Plugin.draftSamplingPeriod!.Value != 0)
+                    return lastDraft;
+
+                SetQueryPositions(rigidbody);
+
+                float averageDraft = 0f;
+                if (SampleDraft(out float seaLevel))
+                {
+                    for (int i = 0; i < sampleNumber; i++)
+                    {
+                        averageDraft += queryResults[i].y - queryPositions[i].y;
+                    }
+                    averageDraft = averageDraft / sampleNumber + seaLevel;
+                }
+                else
+                {
+                    averageDraft = lastDraft;
+#if DEBUG
+                    Debug.LogBuffered($"Failed to sample draft, using last value {averageDraft}");
+#endif
+                }
+
+                lastDraft = Mathf.Clamp(averageDraft, 0.1f, 20f);
+                return lastDraft;
+            }
+
+            private void SetQueryPositions(Rigidbody rigidbody)
+            {
+                var center = rigidbody.position;
+                var down = 100 * rigidbody.transform.InverseTransformDirection(Vector3.down);
                 for (int i = 0; i < sampleNumber; i++)
                 {
-                    averageDraft += queryResults[i].y - queryPositions[i].y;
+                    queryPositions[i] = rigidbody.ClosestPointOnBounds(
+                        center + down + directions[i]
+                    );
                 }
-                averageDraft = averageDraft / sampleNumber + seaLevel;
             }
-            else
+
+            private bool SampleDraft(out float o_seaLevel)
             {
-                averageDraft = lastDraft;
-#if DEBUG
-                Debug.LogBuffered($"Failed to sample draft, using last value {averageDraft}");
-#endif
+                o_seaLevel = OceanRenderer.Instance?.SeaLevel ?? 0f;
+                ICollProvider? collProvider = OceanRenderer.Instance?.CollisionProvider;
+                if (collProvider is null)
+                {
+                    return false;
+                }
+
+                int queryStatus = collProvider.Query(
+                    this.GetHashCode(),
+                    0f,
+                    queryPositions,
+                    queryResults,
+                    null,
+                    null
+                );
+                if (!collProvider.RetrieveSucceeded(queryStatus))
+                {
+                    return false;
+                }
+
+                return true;
             }
-
-            lastDraft = Mathf.Clamp(averageDraft, 0.1f, 20f);
-            return lastDraft;
-        }
-
-        private static void SetQueryPositions(Rigidbody rigidbody)
-        {
-            var center = rigidbody.position;
-            for (int i = 0; i < sampleNumber; i++)
-            {
-                queryPositions[i] = rigidbody.ClosestPointOnBounds(center + directions[i]);
-            }
-        }
-
-        private static bool SampleDraft(int queryHash, out float o_seaLevel)
-        {
-            o_seaLevel = OceanRenderer.Instance?.SeaLevel ?? 0f;
-            ICollProvider? collProvider = OceanRenderer.Instance?.CollisionProvider;
-            if (collProvider is null)
-            {
-                return false;
-            }
-
-            int queryStatus = collProvider.Query(
-                queryHash,
-                0f,
-                queryPositions,
-                queryResults,
-                null,
-                null
-            );
-            if (!collProvider.RetrieveSucceeded(queryStatus))
-            {
-                return false;
-            }
-
-            return true;
         }
     }
 }
