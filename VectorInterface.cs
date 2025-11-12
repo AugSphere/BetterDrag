36e976272d763432b4340a9019d72a7498272b44
AugSphere <augsphere@gmail.com>
2025-11-12 19:10:56 +0100

    Use structs for vector interface

--- VectorInterface.cs
+++ VectorInterface.cs
@@ -15,96 +15,100 @@
         IVector3<T> Cross(IVector3<T> other);
     }
 
-    internal class UnityVector3(UnityEngine.Vector3 vector) : IVector3<UnityEngine.Vector3>
+    internal struct UnityVector3(UnityEngine.Vector3 vector) : IVector3<UnityEngine.Vector3>
     {
         private UnityEngine.Vector3 _vector = vector;
 
-        public UnityEngine.Vector3 Value
+        public readonly UnityEngine.Vector3 Value
         {
             get { return _vector; }
         }
         public float X
         {
-            get { return _vector.x; }
+            readonly get { return _vector.x; }
             set { _vector.x = value; }
         }
 
         public float Y
         {
-            get { return _vector.y; }
+            readonly get { return _vector.y; }
             set { _vector.y = value; }
         }
 
         public float Z
         {
-            get { return _vector.z; }
+            readonly get { return _vector.z; }
             set { _vector.z = value; }
         }
 
-        public float Magnitude
+        public readonly float Magnitude
         {
             get { return _vector.magnitude; }
         }
 
-        public IVector3<UnityEngine.Vector3> Cross(IVector3<UnityEngine.Vector3> other)
+        public readonly IVector3<UnityEngine.Vector3> Cross(IVector3<UnityEngine.Vector3> other)
         {
             return new UnityVector3(UnityEngine.Vector3.Cross(_vector, other.Value));
         }
 
-        public IVector3<UnityEngine.Vector3> Subtract(IVector3<UnityEngine.Vector3> other)
+        public readonly IVector3<UnityEngine.Vector3> Subtract(IVector3<UnityEngine.Vector3> other)
         {
             return new UnityVector3(_vector - other.Value);
         }
 
-        public float Dot(IVector3<UnityEngine.Vector3> other)
+        public readonly float Dot(IVector3<UnityEngine.Vector3> other)
         {
             return UnityEngine.Vector3.Dot(_vector, other.Value);
         }
     }
 
-    internal class NumericsVector3(System.Numerics.Vector3 vector)
+    internal struct NumericsVector3(System.Numerics.Vector3 vector)
         : IVector3<System.Numerics.Vector3>
     {
         private System.Numerics.Vector3 _vector = vector;
 
-        public System.Numerics.Vector3 Value
+        public readonly System.Numerics.Vector3 Value
         {
             get { return _vector; }
         }
         public float X
         {
-            get { return _vector.X; }
+            readonly get { return _vector.X; }
             set { _vector.X = value; }
         }
 
         public float Y
         {
-            get { return _vector.Y; }
+            readonly get { return _vector.Y; }
             set { _vector.Y = value; }
         }
 
         public float Z
         {
-            get { return _vector.Z; }
+            readonly get { return _vector.Z; }
             set { _vector.Z = value; }
         }
 
-        public float Magnitude
+        public readonly float Magnitude
         {
             get { return _vector.Length(); }
         }
 
-        public IVector3<System.Numerics.Vector3> Cross(IVector3<System.Numerics.Vector3> other)
+        public readonly IVector3<System.Numerics.Vector3> Cross(
+            IVector3<System.Numerics.Vector3> other
+        )
         {
             return new NumericsVector3(System.Numerics.Vector3.Cross(_vector, other.Value));
         }
 
-        public IVector3<System.Numerics.Vector3> Subtract(IVector3<System.Numerics.Vector3> other)
+        public readonly IVector3<System.Numerics.Vector3> Subtract(
+            IVector3<System.Numerics.Vector3> other
+        )
         {
             return new NumericsVector3(_vector - other.Value);
         }
 
-        public float Dot(IVector3<System.Numerics.Vector3> other)
+        public readonly float Dot(IVector3<System.Numerics.Vector3> other)
         {
             return System.Numerics.Vector3.Dot(_vector, other.Value);
         }
