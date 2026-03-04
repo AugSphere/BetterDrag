8175b10b66d84d2edd2bd85a30cf7cd6a0050740
AugSphere <augsphere@gmail.com>
2026-03-04 19:51:49 +0100

    Refactor input filters

--- InputFilter.cs
+++ InputFilter.cs
@@ -7,33 +7,20 @@ namespace BetterDrag
         private const float smoothingFactor = 0.25f;
         private readonly Rigidbody rigidBody = rigidBody;
 
-        private readonly Vector3[] smoothedBodyVelocities = new Vector3[
-            Hydrostatics.probeLengthPositions * 2
-        ];
-        private readonly Vector3[] smoothedQueryVelocities = new Vector3[
-            Hydrostatics.probeLengthPositions * 2
-        ];
-        private readonly Vector3[] smoothedQueryDisplacements = new Vector3[
-            Hydrostatics.probeLengthPositions * 2
-        ];
-
-        private readonly OutlierFilter bodyVelocityFilter = new(
-            "body velocity filter",
-            rigidBody.gameObject.name,
-            rateLimit: 1.2f,
-            noFilterCutoff: 0.1f
+        private readonly VectorArrayFilter bodyVelocityFilter = new(
+            "body velocity",
+            rigidBody,
+            0.75f
         );
-        private readonly OutlierFilter queryVelocityFilter = new(
-            "query velocity filter",
-            rigidBody.gameObject.name,
-            rateLimit: 1.2f,
-            noFilterCutoff: 0.1f
+        private readonly VectorArrayFilter queryVelocityFilter = new(
+            "water velocity",
+            rigidBody,
+            0.25f
         );
-        private readonly OutlierFilter queryDisplacementFilter = new(
-            "query displacement filter",
-            rigidBody.gameObject.name,
-            rateLimit: 1.2f,
-            noFilterCutoff: 0.1f
+        private readonly VectorArrayFilter queryDisplacementFilter = new(
+            "displacement",
+            rigidBody,
+            0.75f
         );
 
         internal (
@@ -49,34 +36,57 @@ namespace BetterDrag
         {
             var areInputsValid =
                 !dontUpdateVelocity
-                && !bodyVelocityFilter.IsOutlier(rigidBody.velocity.magnitude)
-                && !queryVelocityFilter.IsAnyMagnitudeOutlier(queryVelocities)
-                && !queryDisplacementFilter.IsAnyMagnitudeOutlier(queryDisplacements);
+                && !bodyVelocityFilter.magnitudeFilter.IsOutlier(rigidBody.velocity.magnitude)
+                && !queryVelocityFilter.magnitudeFilter.IsAnyMagnitudeOutlier(queryVelocities)
+                && !queryDisplacementFilter.magnitudeFilter.IsAnyMagnitudeOutlier(
+                    queryDisplacements
+                );
 
 #if DEBUG
-            BetterDragDebug.LogCSVBuffered([("valid inputs", areInputsValid ? 1 : 0)]);
+            BetterDragDebug.LogCSVBuffered([("valid_inputs", areInputsValid ? 1 : 0)]);
 #endif
 
             if (areInputsValid)
             {
                 for (var idx = 0; idx < Hydrostatics.probeLengthPositions * 2; ++idx)
                 {
-                    SmoothValue(smoothedQueryVelocities, idx, queryVelocities[idx]);
-                    SmoothValue(
-                        smoothedBodyVelocities,
-                        idx,
-                        rigidBody.GetPointVelocity(queryPoints[idx])
+                    bodyVelocityFilter.UpdateValueAtIndex(
+                        rigidBody.GetPointVelocity(queryPoints[idx]),
+                        idx
                     );
-                    SmoothValue(smoothedQueryDisplacements, idx, queryDisplacements[idx]);
+                    queryVelocityFilter.UpdateValueAtIndex(queryVelocities[idx], idx);
+                    queryDisplacementFilter.UpdateValueAtIndex(queryDisplacements[idx], idx);
                 }
             }
-            return (smoothedBodyVelocities, smoothedQueryVelocities, smoothedQueryDisplacements);
+            return (
+                bodyVelocityFilter.smoothedValues,
+                queryVelocityFilter.smoothedValues,
+                queryDisplacementFilter.smoothedValues
+            );
         }
 
         private static void SmoothValue(Vector3[] smoothedValues, int idx, Vector3 value)
         {
             smoothedValues[idx] *= (1f - smoothingFactor);
             smoothedValues[idx] += smoothingFactor * value;
+        }
+
+        private class VectorArrayFilter(string name, Rigidbody rigidBody, float smoothingFactor)
+        {
+            public Vector3[] smoothedValues = new Vector3[Hydrostatics.probeLengthPositions * 2];
+            public readonly OutlierFilter magnitudeFilter = new(
+                $"{name} filter",
+                rigidBody.gameObject.name,
+                rateLimit: 1.2f,
+                noFilterCutoff: 0.1f
+            );
+            private readonly float smoothingFactor = smoothingFactor;
+
+            public void UpdateValueAtIndex(Vector3 value, int idx)
+            {
+                smoothedValues[idx] *= (1f - smoothingFactor);
+                smoothedValues[idx] += smoothingFactor * value;
+            }
         }
     }
 }
