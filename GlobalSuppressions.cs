4cdc959d107f6aca52f145a5a3d866295db6ffc2
AugSphere <augsphere@gmail.com>
2026-07-29 17:22:43 +0200

    Remove style checker

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -6,46 +6,6 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage(
-    "Size",
-    "LongLine:A long line must be avoided.",
-    Justification = "Using CSharpier for formatting",
-    Scope = "module"
-)]
-[assembly: SuppressMessage(
-    "Naming",
-    "Underscore:The name of local variables must not include underscores.",
-    Justification = "Harmony magic needs underscore",
-    Scope = "type",
-    Target = "~T:BetterDrag.BoatProbesFixedUpdateDragPatch"
-)]
-[assembly: SuppressMessage(
-    "Cleaning",
-    "UnusedVariable:Unused variable is declared.",
-    Justification = "Compatibility with delegate",
-    Scope = "type",
-    Target = "~T:BetterDrag.Physics.DragModel"
-)]
-[assembly: SuppressMessage(
-    "Refactoring",
-    "NotOneShotInitialization:Declare the local variable with one-shot initialization.",
-    Justification = "No cleaner way to merge",
-    Scope = "member",
-    Target = "~M:BetterDrag.ShipConfiguration.ShipDragConfigManager.GetPerformanceData(UnityEngine.GameObject)~BetterDrag.ShipConfiguration.ShipDragPerformanceData"
-)]
-[assembly: SuppressMessage(
-    "Cleaning",
-    "ByteOrderMark:The Byte Order Mark (BOM) must be removed.",
-    Scope = "module",
-    Justification = "VS compatibility"
-)]
-[assembly: SuppressMessage(
-    "Cleaning",
-    "UnusedVariable:Unused variable is declared.",
-    Justification = "Conditional compilation",
-    Scope = "type",
-    Target = "~T:BetterDrag.Utilities.Profiler"
-)]
-[assembly: SuppressMessage(
     "Style",
     "IDE0051:Remove unused private members",
     Justification = "Conditional compilation",
@@ -95,20 +55,6 @@ using System.Diagnostics.CodeAnalysis;
     Target = "~F:BetterDrag.Hydrostatics.Hydrostatics._shipName"
 )]
 [assembly: SuppressMessage(
-    "Cleaning",
-    "UnusedVariable:Unused variable is declared.",
-    Justification = "Debug print support",
-    Scope = "member",
-    Target = "~M:BetterDrag.Physics.PhysicsCalculation.CalculateDragForce(System.Single,System.Single,System.Single,System.Single,BetterDrag.ShipConfiguration.ShipDragPerformanceData,System.Boolean,System.Int32)~System.Single"
-)]
-[assembly: SuppressMessage(
-    "Cleaning",
-    "UnusedVariable:Unused variable is declared.",
-    Justification = "API Deprecation",
-    Scope = "member",
-    Target = "~M:BetterDrag.ShipConfiguration.ShipDragPerformanceData.#ctor(System.Single,System.Nullable{System.Single},System.Nullable{System.Single},System.Nullable{System.Single},System.Nullable{System.Single},System.Nullable{System.Single},System.Nullable{System.Single},BetterDrag.ShipConfiguration.ShipDragPerformanceData.DragForceFunction,BetterDrag.ShipConfiguration.ShipDragPerformanceData.DragForceFunction)"
-)]
-[assembly: SuppressMessage(
     "Performance",
     "CA1814:Prefer jagged arrays over multidimensional",
     Justification = "Wastes no space",
@@ -119,12 +65,6 @@ using System.Diagnostics.CodeAnalysis;
     "Style",
     "IDE0008:Use explicit type",
     Justification = "Strict enough without it",
-    Scope = "module"
-)]
-[assembly: SuppressMessage(
-    "Refactoring",
-    "IsNull:Do not use 'is' pattern matching with 'null'.",
-    Justification = "Reference check for null is correct",
     Scope = "module"
 )]
 [assembly: SuppressMessage(
