51cae5bccde1fb5fdb1975ccec629a1787f3930e
AugSphere <augsphere@gmail.com>
2026-08-29 12:39:44 +0200

    Use global coordinate vertical axis for velocity

--- Physics/PhysicsCalculation.cs
+++ Physics/PhysicsCalculation.cs
@@ -111,7 +111,8 @@ internal static class PhysicsCalculation
 
         float seaLevel = OceanRenderer.Instance.SeaLevel;
         Vector3 bodyForward = rigidBody.transform.forward;
-        Vector3 bodyUp = rigidBody.transform.up;
+        bodyForward.y = 0;
+        Vector3 bodyUp = Vector3.up;
 
         for (int idx = 0; idx < boatProbes._forcePoints.Length; ++idx)
         {
